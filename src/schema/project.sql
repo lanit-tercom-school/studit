293753800245167daf6de80df00632e846aeac97
Tytysha <evilina_19@mail.ru>
2016-12-05 21:21:43 +0500

    Add files via upload

---
+++ src/schema/project.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE "project" (
+	"id" serial NOT NULL,
+	"description" TEXT NOT NULL,
+	"tasks" TEXT NOT NULL,
+	"marerials" TEXT NOT NULL,
+	"news" TEXT NOT NULL,
+	CONSTRAINT project_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
