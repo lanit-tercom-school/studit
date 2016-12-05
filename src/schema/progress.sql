f7392c64fd1e029957ac62b0e7c543d527b3ecfd
Tytysha <evilina_19@mail.ru>
2016-12-05 21:47:03 +0500

    Add files via upload

---
+++ src/schema/progress.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE "user_project" (
+	"id" serial NOT NULL,
+	"user_id" bigint NOT NULL,
+	"project_id" bigint NOT NULL,
+	"date" DATE NOT NULL,
+	"progress" int NOT NULL,
+	CONSTRAINT user_project_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "user_course" (
+	"id" serial NOT NULL,
+	"user_id" bigint NOT NULL,
+	"course_id" bigint NOT NULL,
+	"date" DATE NOT NULL,
+	"progress" int NOT NULL,
+	CONSTRAINT user_course_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "user_comments" (
+	"id" serial NOT NULL,
+	"user_id" bigint NOT NULL,
+	"comment_id" bigint NOT NULL,
+	"date" DATE NOT NULL,
+	CONSTRAINT user_comments_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+
