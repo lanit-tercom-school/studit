16ce39fca587d182e415c2b4b4e72568a6ffcce6
Vetcher <vetcher@ngs.ru>
2017-03-03 11:23:04 +0300

    fix installpackages.cmd

--- src/service/InstallPackages.cmd
+++ src/service/InstallPackages.cmd
@@ -2,4 +2,4 @@ go get github.com/astaxie/beego
 go get github.com/beego/bee
 go get github.com/lib/pq
 go get github.com/astaxie/beego/logs
-go get github.com/smartystreets/goconvey/convey
+go get github.com/smartystreets/goconvey
