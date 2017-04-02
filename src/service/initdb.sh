811903c128312c5da7adf365d1ba63281563173a
Vetcher <vetcher@ngs.ru>
2017-04-02 19:45:30 +0300

    One more fix

--- src/service/initdb.sh
+++ src/service/initdb.sh
@@ -3,4 +3,4 @@ export PATH=$PATH:$GOPATH/bin
 export curpath=$PWD
 
 cd schema
-./init.sh > initdb.log & cd $curpath\tests & go test > init_tests.log & cd $curpath
+./init.sh > initdb.log & cd $curpath/tests & go test > init_tests.log & cd $curpath
