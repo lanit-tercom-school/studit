0bd9eaf265125b30de50380fe5ba194a5b656091
Рагозин Роман <eyshella@yandex.ru>
2017-04-21 14:43:09 +0300

    Add deleteProject to home-page-teacher.component.ts

--- webroot/src/app/components/pages/home-page-teacher/home-page-teacher.component.ts
+++ webroot/src/app/components/pages/home-page-teacher/home-page-teacher.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiService} from '../../../services/api.service'
+import { ApiService } from '../../../services/api.service'
 
 @Component({
   selector: 'app-home-page-teacher',
@@ -7,22 +7,25 @@ import {ApiService} from '../../../services/api.service'
   styleUrls: ['./home-page-teacher.component.css']
 })
 export class HomePageTeacherComponent implements OnInit {
-  private description:string;
-  private id:number;
+  private description: string;
+  private id: number;
   private logo: string;
-  private name:string;
-  constructor(private api:ApiService) { }
+  private name: string;
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
-
+    
   }
-  makeProject()
-  {
-    this.name=prompt("Name of project?");
-    this.description=prompt("Description of project?");
-    this.logo=prompt("Logo of project?");
-    this.id=Number(prompt("Id of project?"));
-    let project={'description':this.description,'id':this.id,'logo':this.logo,'name':this.name};
-    this.api.postProject(project,JSON.parse(localStorage.getItem('current_user')).token).subscribe(res=>{});
+  makeProject() {
+    this.name = prompt("Name of project?");
+    this.description = prompt("Description of project?");
+    this.logo = prompt("Logo of project?");
+    this.id = Number(prompt("Id of project?"));
+    let project = { 'description': this.description, 'id': this.id, 'logo': this.logo, 'name': this.name };
+    this.api.postProject(project, JSON.parse(localStorage.getItem('current_user')).token).subscribe(res => { });
+  }
+  deleteProject() {
+    this.id = Number(prompt("Id of project?"));
+    this.api.deleteProject("" + this.id, JSON.parse(localStorage.getItem('current_user')).token).subscribe(res=>{});
   }
 }
