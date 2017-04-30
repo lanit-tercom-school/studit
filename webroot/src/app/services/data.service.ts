67f628aa8e25c48156283b4bd01460e4b26c6ad7
Рагозин Роман <eyshella@yandex.ru>
2017-04-30 12:31:02 +0300

    make getProjectsOfUser() and   getEnrollingProjectsOfUser()

--- webroot/src/app/services/data.service.ts
+++ webroot/src/app/services/data.service.ts
@@ -3,22 +3,46 @@ import { ApiService } from './api.service'
 
 @Injectable()
 export class DataService {
-  private Projects;
-  private userProjects;
+  private projectItems;
+  private projects;
   private enrollingProjects;
   private userId: number;
   constructor(private api: ApiService) { }
   load_data() {
+    this.projects = new Array<number>();
+    this.enrollingProjects = new Array<number>();
     this.userId = JSON.parse(localStorage.getItem('current_user')).id;
     console.log('Data loading...');
     this.api.getProjectItems().subscribe(res => {
-      console.log('Projects:');
-      this.Projects = res;
-      console.log(this.Projects);
-      for (let i = 0; i < this.Projects.length; i++) {
-        
+      this.projectItems = res;
+      for (let i = 0; i < this.projectItems.length; i++) {
+        this.api.getEnrolledUsersToProject(this.projectItems[i].Id).subscribe(res => {
+          if (res.json() != null) {
+            for (let a of res.json()) {
+              if (a === this.userId) {
+                this.enrollingProjects.push(this.projectItems[i].Id);
+                break;
+              }
+            }
+          }
+        });
+        this.api.getProjectUsers(this.projectItems[i].Id).subscribe(res => {
+          if (res.json() != null) {
+            for (let a of res.json()) {
+              if (a === this.userId) {
+                this.projects.push(this.projectItems[i].Id);
+                break;
+              }
+            }
+          }
+        });
       }
-    })
+    });
   }
-  
+  getProjectsOfUser() {
+    return this.projects;
+  }
+  getEnrollingProjectsOfUser() {
+    return this.enrollingProjects;
+  }
 }
