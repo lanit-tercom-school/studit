1f2b61b9960d49aad1e05373642a308b48a48b6d
Рагозин Роман <eyshella@yandex.ru>
2017-05-13 20:06:14 +0300

    Started working with dataservice

--- webroot/src/app/services/data.service.ts
+++ webroot/src/app/services/data.service.ts
@@ -1,36 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { ApiService } from './api.service'
 
 @Injectable()
 export class DataService {
-  private projectItems;
+  //Events and boolean that show what is loaded
+  public projectsUploaded: EventEmitter<any>;
+  private _projectsUploaded: boolean;
+  public usersprojectUploaded: EventEmitter<any>;
+  private _usesprojectUploaded: boolean;
+  public newsUploaded: EventEmitter<any>;
+  private _newsUploaded: boolean;
+  //Data
   private projects;
-  private enrollingProjects;
+  private news;
+  private usersProjects;
+  private enrolledUsersProject;
   private userId: number;
+  //Constructor.
   constructor(private api: ApiService) { }
+  //Load data function.
   loadEnrollingAndUserProjects() {
     this.projects = new Array<number>();
-    this.enrollingProjects = new Array<number>();
+    this.enrolledUsersProject = new Array<number>();
     this.userId = JSON.parse(localStorage.getItem('current_user')).id;
     this.api.getProjectItems().subscribe(res => {
       console.log(res);
-      this.projectItems = res;
-      for (let i = 0; i < this.projectItems.length; i++) {
-        this.api.getEnrolledUsersToProject(this.projectItems[i].id).subscribe(res => {
+      this.projects = res;
+      for (let i = 0; i < this.projects.length; i++) {
+        this.api.getEnrolledUsersToProject(this.projects[i].id).subscribe(res => {
           if (res.json() != null) {
             for (let a of res.json()) {
               if (a === this.userId) {
-                this.enrollingProjects.push(this.projectItems[i].id);
+                this.enrolledUsersProject.push(this.projects[i].id);
                 break;
               }
             }
           }
         });
-        this.api.getProjectUsers(this.projectItems[i].id).subscribe(res => {
+        this.api.getProjectUsers(this.projects[i].id).subscribe(res => {
           if (res.json() != null) {
             for (let a of res.json()) {
               if (a === this.userId) {
-                this.projects.push(this.projectItems[i].id);
+                this.projects.push(this.projects[i].id);
                 break;
               }
             }
@@ -39,10 +50,13 @@ export class DataService {
       }
     });
   }
+  loadProjects() {
+    this.api.postProject
+  }
   getProjectsOfUser() {
     return this.projects;
   }
   getEnrollingProjectsOfUser() {
-    return this.enrollingProjects;
+    return this.enrolledUsersProject;
   }
 }
