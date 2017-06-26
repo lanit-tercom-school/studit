01513eeafcb607f0160f28203664a5c6858742b8
Marimunira <m.pokonechnaya@mail.ru>
2017-06-26 20:16:30 +0400

    Landing page: adaptive design for projects-div

--- webroot/src/app/app-module/components/project-list/project-item/project-item.component.ts
+++ webroot/src/app/app-module/components/project-list/project-item/project-item.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ProjectItem } from 'models/project-item';
 
 @Component({
-  host: { 'class': 'card' },
   selector: 'app-project-item',
   templateUrl: './project-item.component.html',
   styleUrls: ['./project-item.component.css']
