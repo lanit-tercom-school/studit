ec3ab1f899e3af5e681b71c47b8e7df589d8e8ad
Рагозин Роман <eyshella@yandex.ru>
2017-07-03 11:41:48 +0300

    Start working on admin-module

--- webroot/src/app/admin-module/admin.module.ts
+++ webroot/src/app/admin-module/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule
   ],
   declarations: []
 })
