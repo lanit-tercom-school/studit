13c9d534795fb9e97ddbd98bf26eeb5d0eac9a82
Рагозин Роман <eyshella@yandex.ru>
2017-07-10 12:46:47 +0300

    little format code

--- webroot/src/app/app-module/components/student-project-page/student-project-page.component.ts
+++ webroot/src/app/app-module/components/student-project-page/student-project-page.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, OnChanges, ViewContainerRef, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from "rxjs/Observable";
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 import { ApiService } from 'services/api.service';
 import { DataService } from 'services/data.service';
 import { MaterialsItem } from 'models/materials-item';
 import { ProjectItem } from 'models/project-item';
 import { ProjectNewsItem } from 'models/proj-news-item';
 import { TasksItem } from 'models/tasks-item';
-import { Observable } from "rxjs/Observable";
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import 'rxjs/add/operator/filter';
 
@@ -31,11 +32,11 @@ export class StudentProjectPageComponent implements OnInit, OnDestroy {
   private message = 'Please write back soon!';
   private enrollButtonStatus: number = 0;//0 - enrolling,1 - you are in project, 2 - unenrolling
   constructor(private apiService: ApiService,
-   private route: ActivatedRoute,
+    private route: ActivatedRoute,
     private http: Http,
-     private data: DataService,
-) 
-     { }
+    private data: DataService,
+  )
+  { }
 
   ngOnInit() {
     if (localStorage.getItem('current_user')) { this.authorized = true; }
@@ -47,7 +48,7 @@ export class StudentProjectPageComponent implements OnInit, OnDestroy {
       });
     this.getTaskItems();
     if (this.data.PermLvl === 1)
-       this.isTeacher = true;
+      this.isTeacher = true;
 
   }
 
@@ -83,28 +84,28 @@ export class StudentProjectPageComponent implements OnInit, OnDestroy {
       JSON.parse(localStorage.getItem('current_user')).bearer_token, this.message).subscribe(res => {
         this.enrollButtonStatus = 2;
         this.data.loadEnrolledUsersProject();
-       });
-       this.isSuccess = true;
+      });
+    this.isSuccess = true;
   }
   unenroll() {
-        this.isSuccess = false;
+    this.isSuccess = false;
     this.apiService.unenrollToProject(this.projectId,
-    JSON.parse(localStorage.getItem('current_user')).bearer_token).subscribe(res => {
-       this.enrollButtonStatus = 0;
-       this.data.loadEnrolledUsersProject();
+      JSON.parse(localStorage.getItem('current_user')).bearer_token).subscribe(res => {
+        this.enrollButtonStatus = 0;
+        this.data.loadEnrolledUsersProject();
       });
-          this.isSuccess = true;
+    this.isSuccess = true;
   }
 
   choseButtonStatus() {
     this.enrollButtonStatus = 0;
     this.data.UserProjects.subscribe(res => {
-      if (res!=null && res.find(pr => pr.id == this.projectId)) {
+      if (res != null && res.find(pr => pr.id == this.projectId)) {
         this.enrollButtonStatus = 1;
       }
     })
     this.data.UserEnrolledProjects.subscribe(res => {
-      if (res!=null && res.find(pr => pr.id == this.projectId)) {
+      if (res != null && res.find(pr => pr.id == this.projectId)) {
         this.enrollButtonStatus = 2;
       }
     })
