93a77c94b1b2eca6296208f5479dae3d57468465
Marimunira <m.pokonechnaya@mail.ru>
2017-07-19 10:32:21 +0400

    Added packages

--- webroot/src/environments/environment.ts
+++ webroot/src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8081",
-  authUrl: "http://localhost:8082",
+  apiUrl: "http://localhost:8080",
+  authUrl: "http://localhost:8082",  // убрать
 };
