87593ef0168f0033e099e6386a550fd3f1797898
Marimunira <m.pokonechnaya@mail.ru>
2017-07-19 12:23:40 +0400

    Added sign up and validation with GraphQL

--- webroot/src/app/services/auth.service.ts
+++ webroot/src/app/services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
-import { User } from 'models/user';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
 import { environment } from '../../environments/environment';
+
+import { User } from 'models/user';
+import { UserRegister } from 'models/user-register';
 
 @Injectable()
 export class AuthService {
@@ -14,19 +17,9 @@ export class AuthService {
     constructor(private http: Http) { }
 
     authenticatenow(user: User) {
-       /* return this.http.post(environment.authUrl + '/v1/auth/signin/', JSON.stringify(user))
-            .map((response: Response) => {
-                // successful login => getting jwt
-                let res = response.json();
-                if (res && res.bearer_token) {
-                    // save data for keeping user logged in
-                    res.login = user.login;
-                    localStorage.setItem('current_user', JSON.stringify(res));
-                }
-            });*/
         var query: String = 'mutation{';
-        query += 'Auth{ Signin(Login:"'+ user.login;
-        query += '" Password:"'+ user.password + '")';
+        query += 'Auth{ Signin(Login:"' + user.login;
+        query += '" Password:"' + user.password + '")';
         query += '{ DataOfExpiration PermissionLevel Token ';
         query += 'User { Id Avatar Nickname Description }}}}';
         return this.http.get(environment.authUrl + '/graphql?query=' + query)
@@ -46,12 +39,15 @@ export class AuthService {
     }
 
     validate(key: string) {
-        return this.http.get(environment.authUrl + '/v1/auth/signup/?pass=' + key)
+        var query = 'mutation{ Auth{';
+        query += 'Activation(ActivationCode:"' + key + '")';
+        query += '{ Message}}}';
+        return this.http.get(environment.authUrl + '/graphql?query=' + query)
             .catch((error: any) => { return Observable.throw(error) });
     }
 
-    register(user) {
-        var headers = new Headers();
+    register(user: UserRegister) {
+        /*var headers = new Headers();
         headers.append('Content-Type', 'application/json');
         return this.http.post(environment.authUrl + '/v1/auth/signup/', JSON.stringify(user), { headers: headers })
             .map((res: Response) => {
@@ -60,15 +56,29 @@ export class AuthService {
                 else
                     return Observable.throw('no code');
             })
+            .catch((error: any) => { return Observable.throw(error) });*/
+        var query = 'mutation{Auth{Signup';
+        query += '(Login:"' + user.login + '" ';
+        query += 'Password: "' + user.password + '" ';
+        query += 'Nickname: "' + user.nickname + '") ';
+        query += '{ ActivationCode }}}';
+        return this.http.get(environment.authUrl + '/graphql?query=' + query)
+            .map((res: Response) => {
+                var code = res.json().data.Auth.ActivationCode;
+                if (code)
+                    localStorage.setItem('validation_code', code);
+                else
+                    return Observable.throw('no code');
+            })
             .catch((error: any) => { return Observable.throw(error) });
     }
 
     private jwt() {
-        // create authorization-page header with jwt token
-        let currentUser = JSON.parse(localStorage.getItem('current_user'));
-        if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'authorization': 'Bearer ' + currentUser.token });
-            return new RequestOptions({ headers: headers });
-        }
+    // create authorization-page header with jwt token
+    let currentUser = JSON.parse(localStorage.getItem('current_user'));
+    if (currentUser && currentUser.token) {
+        let headers = new Headers({ 'authorization': 'Bearer ' + currentUser.token });
+        return new RequestOptions({ headers: headers });
     }
+}
 }
