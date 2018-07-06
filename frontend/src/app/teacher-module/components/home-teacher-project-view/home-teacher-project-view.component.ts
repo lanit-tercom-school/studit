b843501c693b2788799438c68b19c1e31e619828
Орлова Мария <Manya-bunny@yandex.ru>
2018-07-06 21:21:00 +0300

    Completed a window for entering a description of the news and adding a picture to the news

--- frontend/src/app/teacher-module/components/home-teacher-project-view/home-teacher-project-view.component.ts
+++ frontend/src/app/teacher-module/components/home-teacher-project-view/home-teacher-project-view.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from "rxjs/Observable";
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
 
 import { ProjectItem } from 'models/project-item';
 import { TeacherService } from 'services/teacher.service';
 import { StudentService } from 'services/student.service';
 import { DataService } from 'services/data.service';
-import { EnrollItem } from 'models/enroll-item';
+import { FileService } from 'services/file.service';
 
 @Component({
   selector: 'app-home-teacher-project-view',
@@ -14,12 +16,34 @@ import { EnrollItem } from 'models/enroll-item';
 })
 export class HomeTeacherProjectViewComponent implements OnInit {
   public ProjectList: Observable<ProjectItem[]>;
-  
-  constructor(private teacherService: TeacherService, private data: DataService, private studentService: StudentService) { }
+  public CreatedProject = new ProjectItem();
+  public IsCreated = false;
+  constructor(
+    private router: Router,
+    private teacherService: TeacherService,
+    private data: DataService,
+    private http: Http,
+    private fileService: FileService,
+    private studentService: StudentService
+   ) {};
 
   ngOnInit() {
     this.ProjectList = this.data.UserProjects;
+    this.CreatedProject.Logo = './assets/no_image.png';
   }
-  ngOnDestroy() {
+  
+  load(event) {
+    this.fileService.uploadFiles(event.target.files).subscribe(res => {
+      this.CreatedProject.Logo = res;
+    });
   }
+
+  makeProject() {
+    this.teacherService.postProject(this.CreatedProject, JSON.parse(localStorage.getItem('current_user')).Token)
+      .subscribe(() => {
+        console.log('Project was added');
+        this.IsCreated = true;
+        // this.router.navigate(['/home']);
+      });
+    }
 }
